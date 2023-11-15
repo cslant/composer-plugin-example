28ae07e04f284fefcfe84023d0653e8249c0e979
Tan Nguyen <tannp27@gmail.com>
2023-11-15 10:45:38 +0700

    update plugin

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,1 +1,39 @@
+<?php
 
+namespace Cslant\ComposerPlugin\Example;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        echo __METHOD__ . "\n";
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-install-cmd' => 'installOrUpdate',
+            'post-update-cmd' => 'installOrUpdate',
+        );
+    }
+
+    public function installOrUpdate($event)
+    {
+        echo __METHOD__ . "\n";
+        echo get_class($event) . "\n";
+        echo $event->getName() . "\n";
+    }
+}
